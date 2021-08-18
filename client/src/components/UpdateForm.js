899ec3884f70ab90bb575715eed17609cca6472e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-18 12:55:02 -0400

    lecture work-in-progress

--- client/src/components/UpdateForm.js
+++ client/src/components/UpdateForm.js
@@ -14,7 +14,7 @@ const UpdateForm = props => {
   const [item, setItem] = useState(initialItem);
   const id = 1;
 
-  console.log(props);
+  console.log(props.match.params);
 
   const changeHandler = ev => {
     ev.persist();
