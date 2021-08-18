4874e35a757aef2c2a98f26d3011c51d3fc8b871
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-18 12:51:47 -0400

    lecture work-in-progress

--- client/src/components/UpdateForm.js
+++ client/src/components/UpdateForm.js
@@ -13,6 +13,8 @@ const initialItem = {
 const UpdateForm = props => {
   const [item, setItem] = useState(initialItem);
 
+  const id = 1;
+
   const changeHandler = ev => {
     ev.persist();
     let value = ev.target.value;
@@ -27,7 +29,7 @@ const UpdateForm = props => {
   };
 
   useEffect(()=>{
-    axios.get(`http://localhost:3333/items/1`)
+    axios.get(`http://localhost:3333/items/${id}`)
       .then(res=> {
         setItem(res.data);
       })
